7057364cc1ee53b04bd9993f332e8d0ee4878a9e
Joseph Richard <joseph@alterra.id>
2019-02-14 15:04:30 +0700

    Inisiasi Setting-Url Kapanlagi dan View-Model Home

--- Home/views.py
+++ Home/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.db import models
+from django.db.models import CharField
+
+from django.shortcuts import render,redirect
+from .models import Lagu
+from .forms import PostForm
 
 # Create your views here.
+def BarangPost(request):
+    Lagus = Lagu.objects.all()
+    return render(request, 'Lagu.html', {'Lagus':Lagus})
+
+def Lagu_Input(request):
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect(LaguPost)
+    else:
+        form = PostForm()
+        return render(request, 'LaguInput.html', {'form': form})
+
+def Lagu_Detail(request, barang_id):
+    Lagus = Lagu.objects.get(pk=barang_id)
+    return render(request, 'LaguDetail.html', {'Lagus':Lagu})
--- KapanLagi/urls.py
+++ KapanLagi/urls.py
@@ -18,4 +18,10 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('Home.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),
+    ]
