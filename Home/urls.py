8f997695e9ef6ca5ccf7755340d55ce7fa4c0259
Hilman <hilman@alphatech.id>
2019-02-14 15:43:56 +0700

    Percobaan Halaman Home

--- Home/urls.py
+++ Home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.tampilan_toko, name='home'),
-    path('barang/<int:toko_id>', views.toko_detail, name='toko_detail'),
+    path('', views.LaguPost, name='home'),
+    #path('barang/<int:toko_id>', views.LaguDetail, name='lagu_detail'),
 ]
