b7935aee94a73e951bf303858aff8c43581c81aa
Joseph Richard <joseph@alterra.id>
2019-02-14 22:43:52 +0700

    Progress user log in log out protected page

--- Home/urls.py
+++ Home/urls.py
@@ -6,8 +6,8 @@ from Home import views as views1
 urlpatterns = [
     path('', views.Lagu_Post, name='home'),
     path('input/', views.Lagu_Input, name='Lagu_Input'),
-    path('signup/', views.Lagu_Signup, name='LaguSignUp'),
-    path('signin/', views.Lagu_Signin, name='LaguSignIn'),
+    path('signup/', views.Lagu_SignUp, name='LaguSignUp'),
+    path('signin/', views.Lagu_SignIn, name='LaguSignIn'),
     path('signout/', views.Lagu_Signout, name='LaguSignOut'),
     path('lirik/', views.LirikLagu, name='lirik'),
     path('print/<int:lagu_id>', views.PrintDetail, name='print'),
